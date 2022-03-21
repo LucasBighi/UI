4bbe0f5b7a65c7d12197e8ef912d3121f50ef79b
Lucas Bighi <lucas.bighi@turbobank.co>
2022-03-21 16:43:20 -0300

    token

--- Sources/UI/Components/Labels/Label.swift
+++ Sources/UI/Components/Labels/Label.swift
@@ -66,21 +66,3 @@ public class Label: UILabel {
         }
     }
 }
-
-extension String {
-    func slice(from: String, to: String) -> String? {
-        guard let rangeFrom = range(of: from)?.upperBound else { return nil }
-        guard let rangeTo = self[rangeFrom...].range(of: to)?.lowerBound else { return nil }
-        return String(self[rangeFrom..<rangeTo])
-    }
-}
-
-//extension String {
-//    func slice(from: String, to: String) -> String? {
-//        return (range(of: from)?.upperBound).flatMap { substringFrom in
-//            (range(of: to, range: substringFrom..<endIndex)?.lowerBound).map { substringTo in
-//                String(self[substringFrom..<substringTo])
-//            }
-//        }
-//    }
-//}
